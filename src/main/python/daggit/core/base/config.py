f1d1df9ba2be8e7806361231b0caa74b1dda739a
Sanjay Renduchintala <san8055@gmail.com>
2020-10-08 23:43:17 -0400

    change default intermediate saves to csv from h5

--- src/main/python/daggit/core/base/config.py
+++ src/main/python/daggit/core/base/config.py
@@ -1,5 +1,5 @@
 DAGGIT_HOME = "DAGGIT_HOME"
 STORE = 'Local'
 ORCHESTRATOR = 'airflow'
-STORAGE_FORMAT = '.h5'
+STORAGE_FORMAT = '.csv'
 ORCHESTRATOR_ATTR = dict({'AIRFLOW': dict({'dag_config':dict({'depends_on_past': False, 'schedule_interval':'@once','start_date':'20-11-2018'})})})
